63f783bc3a65d94101907cf325e26d63fa911443
night0721 <77528305+night0721@users.noreply.github.com.>
2023-09-24 00:58:00 +0100

    layout and status circle fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,21 @@ import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width" />
       </Head>
-      <Layout Component={Component} pageProps={pageProps} />
+      <Component {...pageProps} />
     </>
   );
 }
 
-const Layout = ({ Component, pageProps, router }: AppProps) => {
-   if (Component.getLayout) {
-     return Component.getLayout(<Component {...pageProps} />);
-   } else {
-     return <Component {...pageProps} />;
-   }
- };
+// const Layout = ({ Component, pageProps, router }: AppProps) => {
+//   if (Component.getLayout) {
+//     return Component.getLayout(<Component {...pageProps} />);
+//   } else {
+//     return <Component {...pageProps} />;
+//   }
+// };
