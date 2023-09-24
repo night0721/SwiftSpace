c3dcf613b0660d46e9005e12b464e356b2c632ff
night0721 <77528305+night0721@users.noreply.github.com.>
2023-09-24 01:35:05 +0100

    layout fix for ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,24 @@ export default function App({ Component, pageProps, router }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      {/* <Component {...pageProps} /> */}
+        <Layout Component={Component} pageProps={pageProps} router={router} />
     </>
   );
 }
 
-// const Layout = ({ Component, pageProps, router }: AppProps) => {
-//   if (Component.getLayout) {
-//     return Component.getLayout(<Component {...pageProps} />);
-//   } else {
-//     return <Component {...pageProps} />;
-//   }
-// };
+// Define an interface that extends the Component type
+interface ExtendedComponent extends React.FC<AppProps> {
+  getLayout?: (page: JSX.Element) => JSX.Element;
+}
+
+const Layout = ({ Component, pageProps }: AppProps) => {
+  // Use the ExtendedComponent type here
+  const ExtendedComponent = Component as ExtendedComponent;
+
+  if (ExtendedComponent.getLayout) {
+    return ExtendedComponent.getLayout(<Component {...pageProps} />);
+  } else {
+    return <Component {...pageProps} />;
+  }
+};
