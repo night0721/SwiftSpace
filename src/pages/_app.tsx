fcf3c0e9896dbd31a1dd5ba3df2dde6e2e3f93b5
night0721 <77528305+night0721@users.noreply.github.com.>
2023-10-19 00:29:34 +0100

    sync

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { Router } from "next/router";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
