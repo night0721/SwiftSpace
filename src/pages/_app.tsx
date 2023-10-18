a9faa4d94a95b441a94b88addc8e403496c42d8b
night0721 <77528305+night0721@users.noreply.github.com.>
2023-10-19 00:40:23 +0100

    app fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
-import { AppProps } from "next/app";
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
 import Head from "next/head";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+ 
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
@@ -8,24 +18,28 @@ export default function App({ Component, pageProps, router }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width" />
       </Head>
-      {/* <Component {...pageProps} /> */}
-        <Layout Component={Component} pageProps={pageProps} router={router} />
+      <Layout Component={Component} pageProps={pageProps} />
     </>
   );
 }
 
 // Define an interface that extends the Component type
-interface ExtendedComponent extends React.FC<AppProps> {
-  getLayout?: (page: JSX.Element) => JSX.Element;
-}
+//interface ExtendedComponent extends React.FC<AppProps> {
+//  getLayout?: (page: JSX.Element) => JSX.Element;
+//}
 
-const Layout = ({ Component, pageProps }: AppProps) => {
-  // Use the ExtendedComponent type here
-  const ExtendedComponent = Component as ExtendedComponent;
-
-  if (ExtendedComponent.getLayout) {
-    return ExtendedComponent.getLayout(<Component {...pageProps} />);
-  } else {
-    return <Component {...pageProps} />;
-  }
+const Layout = ({ Component, pageProps }: AppPropsWithLayout) => {
+   // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+ 
+  return getLayout(<Component {...pageProps} />)
 };
+
+ 
+ 
+//export default function App({ Component, pageProps }: AppPropsWithLayout) {
+//  // Use the layout defined at the page level, if available
+//  const getLayout = Component.getLayout ?? ((page) => page)
+// 
+//  return getLayout(<Component {...pageProps} />)
+//}
